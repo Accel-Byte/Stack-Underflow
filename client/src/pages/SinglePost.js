501d21e18a99e91c755edc7a73b7797681be92bd
d3v13 <patelcheris13@gmail.com>
2021-03-29 11:34:22 +0530

    Added Answers and Editor to write Answer

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -1,33 +1,17 @@
-import React, { useContext, useState, useRef } from 'react';
+import React, { useContext } from 'react';
 import { gql } from '@apollo/client';
-import { useQuery, useMutation } from '@apollo/client';
-import moment from 'moment';
-import {
-  Button,
-  Card,
-  Form,
-  Grid,
-  Image,
-  Icon,
-  Label,
-  Divider,
-} from 'semantic-ui-react';
+import { useQuery } from '@apollo/client';
+import { Card, Grid, Image, Divider } from 'semantic-ui-react';
 
 import { AuthContext } from '../context/auth';
 import VoteButton from '../components/Button/LikeButton';
-import DisLikeButton from '../components/Button/DisLikeButton';
-import DeleteButton from '../components/DeleteButton';
-import MyPopup from '../utils/MyPopup';
-import CreateComment from '../components/comment/createComment';
 import Editor from '../components/Editor';
+import Answer from '../components/Answer';
+import Question from '../components/Question';
 
 function SinglePost(props) {
   const postId = props.match.params.postId;
-
   const { user } = useContext(AuthContext);
-  const commentInputRef = useRef(null);
-  const [open, setOpen] = useState(false);
-  const [comment, setComment] = useState('');
 
   const { data: { getPost } = {} } = useQuery(FETCH_POST_QUERY, {
     variables: {
@@ -35,43 +19,17 @@ function SinglePost(props) {
     },
   });
 
-  const [submitComment] = useMutation(SUBMIT_COMMENT_MUTATION, {
-    update() {
-      setComment('');
-      commentInputRef.current.blur();
-      setOpen(false);
-    },
-    variables: {
-      postId,
-      body: comment,
-    },
-    onError(err) {
-      console.log(err);
-    },
-  });
-
-  function deletePostCallback() {
-    props.history.push('/');
-  }
-
   let postMarkup;
   if (!getPost) {
     postMarkup = <p>Loading post..</p>;
   } else {
-    const {
-      id,
-      body,
-      createdAt,
-      username,
-      comments,
-      voteCount,
-      upvotes,
-      downvotes,
-      commentCount,
-    } = getPost;
-    const commentChange = (e) => {
-      setComment(e.target.value);
-    };
+    const { id } = getPost;
+    const question = getPost.question;
+    const answers = getPost.answers;
+
+    function deletePostCallback() {
+      props.history.push('/');
+    }
     postMarkup = (
       <Grid>
         <Grid.Column width={3}>
@@ -86,109 +44,61 @@ function SinglePost(props) {
             centered
           ></Image>
         </Grid.Column>
-        <Grid.Column width={1} style={{paddingTop:"1rem"}}>
-          <VoteButton
-            user={user}
-            post={{ id, voteCount, upvotes, downvotes }}
-          />
-        </Grid.Column>
-        <Grid.Column width={12} style={{paddingLeft:"0"}}>
+        <Grid.Column width={13}>
           <Grid.Row>
-            <Card fluid color="blue" style={{ padding: '1rem' }}>
-              <Card.Content>
-                <Card.Header>{body}</Card.Header>
-                <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
-                <Card.Description
-                  style={{
-                    marginBottom: '20px',
-                    marginTop: '0px',
-                    color: '#999',
-                  }}
-                >
-                  {'By ' + username}
-                </Card.Description>
-                <Divider />
-                <Card.Description style={{ fontSize: '16px', color: '#000' }}>
-                  Velit occaecat dolor nulla ullamco velit duis dolore aliqua
-                  nisi. Sunt excepteur ullamco duis aliquip nostrud laborum
-                  minim est ut. Tempor nulla dolor elit proident.Consectetur
-                  aliqua id pariatur pariatur aliquip minim aliquip laborum
-                  labore do proident proident laborum nostrud. Cillum sit duis
-                  magna pariatur duis. Qui id consectetur amet culpa sunt ea
-                  nostrud ex sit quis velit sunt. Cillum fugiat nostrud qui elit
-                  ex cillum culpa.
-                </Card.Description>
-              </Card.Content>
-              <hr />
-              <Card.Content extra>
-               <MyPopup content="Comment on post">
-                  <Button
-                    compact
-                    size="mini"
-                    as="div"
-                    labelPosition="right"
-                    onClick={() => console.log('Comment on post')}
-                  >
-                    <Button compact size="mini" basic color="blue">
-                      <Icon name="comments" />
-                    </Button>
-                    <Label basic color="blue" pointing="left">
-                      {commentCount}
-                    </Label>
-                  </Button>
-                </MyPopup>
-                {user && user.username === username && (
-                  <DeleteButton postId={id} callback={deletePostCallback} />
-                )}
-                <CreateComment
-                  submitComment={submitComment}
-                  comment={comment}
+            <Grid>
+              <Grid.Column width={1} style={{ paddingTop: '1rem' }}>
+                <VoteButton
                   user={user}
-                  commentChange={commentChange}
-                  commentInputRef={commentInputRef}
-                  open={open}
-                  setOpen={setOpen}
+                  id={id}
+                  voteCount={question.voteCount}
+                  upvotes={question.upvotes}
+                  downvotes={question.downvotes}
                 />
-              </Card.Content>
-              <Card.Content>
-                {comments.map((comment) => (
-                  <Card fluid key={comment.id}>
-                    <Card.Content style={{ padding: '4px 10px 4px 10px' }}>
-                      {user && user.username === comment.username && (
-                        <DeleteButton postId={id} commentId={comment.id} />
-                      )}
-                      <Card.Header
-                        style={{
-                          display: 'inline',
-                          marginRight: '5px',
-                          fontSize: '12px',
-                        }}
-                      >
-                        {comment.username}
-                      </Card.Header>
-                      <Card.Meta
-                        style={{ display: 'inline', fontSize: '12px' }}
-                      >
-                        {moment(comment.createdAt).fromNow()}
-                      </Card.Meta>
-                      <Card.Description style={{ fontSize: '14px' }}>
-                        {comment.body}
-                      </Card.Description>
-                    </Card.Content>
-                  </Card>
+              </Grid.Column>
+              <Grid.Column width={15}>
+                <Question
+                  post={getPost}
+                  user={user}
+                  deletePostCallback={deletePostCallback}
+                />
+              </Grid.Column>
+            </Grid>
+            <br />
+            <Divider />
+            <Grid.Row>
+              <Grid>
+                <Grid.Row>
+                  <h2 style={{ paddingLeft: '1rem' }}>
+                    {answers.length} Answers
+                  </h2>
+                </Grid.Row>
+                {answers.map((answer) => (
+                  <Grid.Row key={answer.id}>
+                    <Answer answer={answer} user={user} id={id} />
+                  </Grid.Row>
                 ))}
-              </Card.Content>
-            </Card>
-          </Grid.Row>
-          <br />
-          <Grid.Row>
-            <Grid.Column>
-              <Card fluid color="yellow" style={{ padding: '10px' }}>
-                <Card.Content>
-                  <Editor />
-                </Card.Content>
-              </Card>
-            </Grid.Column>
+              </Grid>
+            </Grid.Row>
+            <br />
+            <Divider />
+            <Grid.Row>
+              <Grid>
+                <Grid.Column width={1}></Grid.Column>
+                <Grid.Column width={15}>
+                  <Grid.Row>
+                    <h2 style={{ paddingBottom: '1rem' }}>Your Answer : </h2>
+                  </Grid.Row>
+                  <Grid.Row>
+                    <Card fluid color="yellow" style={{ padding: '10px' }}>
+                      <Card.Content>
+                        <Editor id={id} />
+                      </Card.Content>
+                    </Card>
+                  </Grid.Row>
+                </Grid.Column>
+              </Grid>
+            </Grid.Row>
           </Grid.Row>
         </Grid.Column>
       </Grid>
@@ -197,41 +107,44 @@ function SinglePost(props) {
   return postMarkup;
 }
 
-const SUBMIT_COMMENT_MUTATION = gql`
-  mutation createComment($postId: ID!, $body: String!) {
-    createComment(postId: $postId, body: $body) {
-      id
-      comments {
-        id
-        body
-        createdAt
-        username
-      }
-      commentCount
-    }
-  }
-`;
-
 const FETCH_POST_QUERY = gql`
   query($postId: ID!) {
     getPost(postId: $postId) {
       id
-      body
       createdAt
-      username
-      voteCount
-      upvotes {
+      question {
+        commentCount
         username
-      }
-      downvotes {
-        username
-      }
-      commentCount
-      comments {
-        id
-        username
-        createdAt
         body
+        title
+        upvotes {
+          username
+        }
+        downvotes {
+          username
+        }
+        voteCount
+        comments {
+          id
+          username
+          body
+          createdAt
+        }
+      }
+      answers {
+        id
+        body
+        createdAt
+        upvotes {
+          username
+          createdAt
+        }
+        downvotes {
+          username
+          createdAt
+        }
+        voteCount
+        username
       }
     }
   }
