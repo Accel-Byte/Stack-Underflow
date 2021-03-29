99552ae385bce9660531c76123dd5d52c71d0f18
d3v13 <patelcheris13@gmail.com>
2021-03-29 11:43:55 +0530

    Display All Answers and Question

---
+++ client/src/components/Answer.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Card, Grid } from 'semantic-ui-react';
+import moment from 'moment';
+
+import VoteButton from '../components/Button/LikeButton';
+
+const Answer = ({ answer, user, id }) => {
+  return (
+    <>
+      <Grid.Column  width={1} style={{ paddingTop: '1rem' }}>
+        <VoteButton
+          user={user}
+          id={id}
+          answerId={answer.id}
+          voteCount={answer.voteCount}
+          upvotes={answer.upvotes}
+          downvotes={answer.downvotes}
+        />
+      </Grid.Column>
+      <Grid.Column width={15}>
+        <Card fluid color="purple" style={{ padding: '10px' }}>
+          <Card.Content>
+            <Card.Description style={{ fontSize: '16px', color: '#000' }} dangerouslySetInnerHTML={{ __html: answer.body }}>    
+            </Card.Description>
+            <Card.Meta textAlign="right">
+              {moment(answer.createdAt).fromNow()}
+            </Card.Meta>
+            <Card.Description
+              textAlign="right"
+              style={{
+                marginBottom: '0',
+                marginTop: '0px',
+                color: '#999',
+              }}
+            >
+              {'By ' + answer.username}
+            </Card.Description>
+          </Card.Content>
+        </Card>
+      </Grid.Column>
+    </>
+  );
+};
+
+export default Answer;
---
+++ client/src/components/Question.js
@@ -0,0 +1,127 @@
+import React, { useState, useRef } from 'react';
+import { useMutation } from '@apollo/client';
+import { gql } from '@apollo/client';
+import { Button, Card, Icon, Label, Divider } from 'semantic-ui-react';
+import moment from 'moment';
+
+import DeleteButton from '../components/DeleteButton';
+import MyPopup from '../utils/MyPopup';
+import CreateComment from '../components/comment/createComment';
+import ShowComment from '../components/comment/showComment';
+
+function Question({ post, user, deletePostCallback }) {
+  const commentInputRef = useRef(null);
+  const [open, setOpen] = useState(false);
+  const [comment, setComment] = useState('');
+  const [submitComment] = useMutation(SUBMIT_COMMENT_MUTATION, {
+    update() {
+      setComment('');
+      commentInputRef.current.blur();
+      setOpen(false);
+    },
+    variables: {
+      postId: post.id,
+      body: comment,
+    },
+    onError(err) {
+      console.log(err);
+    },
+  });
+  const commentChange = (e) => {
+    setComment(e.target.value);
+  };
+
+  return (
+    <Card fluid color="blue" style={{ padding: '1rem' }}>
+      <Card.Content>
+        <Card.Header>{post.question.title}</Card.Header>
+        <Card.Meta>{moment(post.createdAt).fromNow()}</Card.Meta>
+        <Card.Description
+          style={{
+            marginBottom: '20px',
+            marginTop: '0px',
+            color: '#999',
+          }}
+        >
+          {'By ' + post.question.username}
+        </Card.Description>
+        <Divider />
+        <Card.Description style={{ fontSize: '16px', color: '#000' }}>
+          {post.question.body}
+        </Card.Description>
+      </Card.Content>
+      <hr />
+      <Card.Content extra>
+        <MyPopup content="Comment on post">
+          <Button
+            compact
+            size="mini"
+            as="div"
+            labelPosition="right"
+            onClick={() => console.log('Comment on post')}
+          >
+            <Button compact size="mini" basic color="blue">
+              <Icon name="comments" />
+            </Button>
+            <Label basic color="blue" pointing="left">
+              {post.question.commentCount}
+            </Label>
+          </Button>
+        </MyPopup>
+        {
+          // TODO: SEE This Bastard Your DownVote button actually Upvote the post
+        }
+        {user && user.username === post.question.username && (
+          <DeleteButton postId={post.id} callback={deletePostCallback} />
+        )}
+        <CreateComment
+          submitComment={submitComment}
+          comment={comment}
+          user={user}
+          commentChange={commentChange}
+          commentInputRef={commentInputRef}
+          open={open}
+          setOpen={setOpen}
+        />
+      </Card.Content>
+      <Card.Content>
+        {post.question.comments.map((comment) => (
+          <ShowComment comment={comment} id={post.id} user={user} />
+        ))}
+      </Card.Content>
+    </Card>
+  );
+}
+
+const SUBMIT_COMMENT_MUTATION = gql`
+  mutation createComment($postId: ID!, $body: String!) {
+    createComment(postId: $postId, body: $body) {
+      id
+      createdAt
+      question {
+        username
+        body
+        title
+        upvotes {
+          username
+        }
+        downvotes {
+          username
+        }
+        voteCount
+        comments {
+          id
+          body
+          username
+        }
+      }
+      answers {
+        id
+        body
+        createdAt
+      }
+    }
+  }
+`;
+
+export default Question;
