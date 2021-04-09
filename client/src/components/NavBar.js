3a2114205a75f4b6db3461d4955510ccdab7e5e2
d3v13 <patelcheris13@gmail.com>
2021-04-09 17:43:40 +0530

    Updated NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,57 +1,60 @@
-import React, {  useState, useContext } from "react";
-import { Menu } from "semantic-ui-react";
-import { Link } from "react-router-dom";
+import React, { useState, useContext } from 'react';
+import { Menu, Segment } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 import { AuthContext } from '../context/auth';
 
 const NavBar = () => {
-
-  const { user, logout } = useContext(AuthContext);  
+  const { user, logout } = useContext(AuthContext);
   const pathname = window.location.pathname;
 
-  const path = pathname === "/" ? "home" : pathname.substr(1);
+  const path = pathname === '/' ? 'home' : pathname.substr(1);
   const [activeItem, setActiveItem] = useState(path);
 
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
-  const navBar =
-  user ? (
-    <Menu pointing secondary size="massive" color="teal">
-      <Menu.Item name={user.username} active as={Link} to="/" />
-
-      <Menu.Menu position="right">
-        <Menu.Item name="logout" onClick={logout} />
-      </Menu.Menu>
-    </Menu>
-  ) : (
-    <Menu pointing secondary size="massive" color="teal">
-      <Menu.Item
-        name="home"
-        active={activeItem === 'home'}
-        onClick={handleItemClick}
-        as={Link}
-        to="/"
-      />
-
-      <Menu.Menu position="right">
-        <Menu.Item
-          name="login"
-          active={activeItem === 'login'}
-          onClick={handleItemClick}
-          as={Link}
-          to="/login"
-        />
-        <Menu.Item
-          name="register"
-          active={activeItem === 'register'}
-          onClick={handleItemClick}
-          as={Link}
-          to="/register"
-        />
-      </Menu.Menu>
-    </Menu>
+  const navBar = (
+    <Segment inverted style={{ borderRadius: 0 }}>
+      {user ? (
+        <Menu inverted size="massive">
+          <Menu.Item name={'StackUnderflow'} onClick={handleItemClick} as={Link} to="/" />
+          <Menu.Menu position="right">
+            <Menu.Item
+              color={'orange'}
+              name={'Dashboard'}
+              active={activeItem === 'Dashboard'}
+              onClick={handleItemClick}
+              as={Link}
+              to="/dashboard"
+            />
+            <Menu.Item color={'red'} name="logout" onClick={logout} />
+          </Menu.Menu>
+        </Menu>
+      ) : (
+        <Menu inverted size="massive">
+          <Menu.Item name={'StackUnderflow'} onClick={handleItemClick} as={Link} to="/" />
+          <Menu.Menu position="right">
+            <Menu.Item
+              color={'green'}
+              name="login"
+              active={activeItem === 'login'}
+              onClick={handleItemClick}
+              as={Link}
+              to="/login"
+            />
+            <Menu.Item
+              color={'green'}
+              name="register"
+              active={activeItem === 'register'}
+              onClick={handleItemClick}
+              as={Link}
+              to="/register"
+            />
+          </Menu.Menu>
+        </Menu>
+      )}
+    </Segment>
   );
-
   return navBar;
 };
 
