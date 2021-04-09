3b3dc40095300757ecba7958f340ab7cbeb4a6e5
d3v13 <patelcheris13@gmail.com>
2021-04-09 23:12:08 +0530

    Seperated HomeCards and DashboardCards

---
+++ client/src/components/PostCard/HomeCard.js
@@ -0,0 +1,54 @@
+import React, { useContext } from 'react';
+import { Button, Icon, Label, Item } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+
+import { AuthContext } from '../../context/auth';
+import DeleteButton from '../DeleteButton';
+import MyPopup from '../../utils/MyPopup';
+
+function PostCard({
+  post: { createdAt, id, question },
+}) {
+  const { user } = useContext(AuthContext);
+  return (
+    <Item.Group divided style={{ border: '1px solid #ddd', padding: '25px' }}>
+      <Item>
+        <Item.Image
+          key={new Date()}
+          size="tiny"
+          src={'https://picsum.photos/200'}
+        />
+
+        <Item.Content>
+          <Item.Header as={Link} to={`/posts/${id}`}>
+            {question.title}
+          </Item.Header>
+          <Item.Meta>{moment(createdAt).fromNow(true) + ' ago'}</Item.Meta>
+          <Item.Description>{question.username}</Item.Description>
+          <Item.Extra>
+            <MyPopup content="Comment on post">
+              <Button
+                size="mini"
+                compact
+                labelPosition="right"
+                as={Link}
+                to={`/posts/${id}`}
+              >
+                <Button size="mini" compact color="blue" basic>
+                  <Icon name="comments" />
+                </Button>
+                <Label basic color="blue" pointing="left">
+                  {question.commentCount}
+                </Label>
+              </Button>
+            </MyPopup>
+            {user && user.username === question.username && <DeleteButton postId={id} />}
+          </Item.Extra>
+        </Item.Content>
+      </Item>
+    </Item.Group>
+  );
+}
+
+export default PostCard;
