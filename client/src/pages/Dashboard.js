f791b3653171ecdcd533d1f204dc70267c6ce625
d3v13 <patelcheris13@gmail.com>
2021-04-09 23:09:13 +0530

    Display All Questions Of User In Dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,11 +1,127 @@
-import React from 'react'
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+import {
+  Grid,
+  Transition,
+  Divider,
+  Image,
+  Card,
+  Icon,
+} from 'semantic-ui-react';
+import moment from 'moment';
 
-function Dashboard() {
-    return (
-        <div>
-            Dashboard
-        </div>
-    )
-}
+import DashboardCard from '../components/PostCard/DashboardCard';
 
-export default Dashboard
+const Dashboard = (props) => {
+  const userId = props.match.params.userId;
+  const {
+    loading,
+    data: { getUser: user, getUserPost: posts } = {},
+  } = useQuery(FETCH_DASHBOARD_DATA, {
+    variables: {
+      userId,
+    },
+  });
+
+  return (
+    <Grid>
+      <Grid.Column width={3}>
+        <Grid.Row>
+          <Card>
+            <Image
+              src="https://picsum.photos/200"
+              label={{
+                content: 'Community',
+                icon: 'users',
+                color: 'purple',
+                ribbon: true,
+              }}
+              wrapped
+              ui={false}
+            />
+            <Card.Content>
+              <Card.Header>{user ? user.username : ''}</Card.Header>
+              <Card.Meta>Joined in {moment(user ? user.createdAt : '').year()}</Card.Meta>
+              <Card.Description>
+                Daniel is a comedian living in Nashville.
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a href="/    ">
+                <Icon name="user" />
+                10 Friends
+              </a>
+            </Card.Content>
+          </Card>
+        </Grid.Row>
+      </Grid.Column>
+      <Grid.Column width={13}>
+        <Divider />
+        <Grid.Row>
+          <Grid>
+            <Grid.Row className="page-title">
+              <h1>All Posts</h1>
+            </Grid.Row>
+            {loading ? (
+              <h1>Loading posts..</h1>
+            ) : (
+              <Transition.Group>
+                {posts &&
+                  posts.map((post) => (
+                    <Grid.Row
+                      key={post.id}
+                      style={{ marginBottom: 20, padding: '0px 10rem' }}
+                    >
+                      <DashboardCard post={post} />
+                    </Grid.Row>
+                  ))}
+              </Transition.Group>
+            )}
+          </Grid>
+        </Grid.Row>
+      </Grid.Column>
+      {/* <Grid columns={3}>
+      <Grid.Row className="page-title">
+        <h1>All Posts</h1>
+      </Grid.Row>
+      {loading ? (
+        <h1>Loading posts..</h1>
+      ) : (
+        <Transition.Group>
+          {posts &&
+            posts.map((post) => (
+              <Grid.Row
+                key={post.id}
+                style={{ marginBottom: 20, padding: '0px 10rem' }}
+              >
+                <PostCard post={post} />
+              </Grid.Row>
+            ))}
+        </Transition.Group>
+      )}
+    </Grid> */}
+    </Grid>
+  );
+};
+
+export default Dashboard;
+
+const FETCH_DASHBOARD_DATA = gql`
+  query DashboardData($userId: ID!) {
+    getUser(userId: $userId) {
+      username
+      email
+      createdAt
+    }
+    getUserPost(userId: $userId) {
+      id
+      createdAt
+      question {
+        commentCount
+        username
+        body
+        title
+      }
+    }
+  }
+`;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,7 +4,7 @@ import { useQuery } from '@apollo/client';
 import { Grid, Transition, Button } from 'semantic-ui-react';
 
 import { AuthContext } from '../context/auth';
-import PostCard from '../components/PostCard';
+import PostCard from '../components/PostCard/HomeCard';
 import { FETCH_POSTS_QUERY } from '../utils/graphql';
 
 function Home() {
@@ -19,25 +19,28 @@ function Home() {
         <h1>Recent Posts</h1>
       </Grid.Row>
       {user && (
-        <Grid.Row style={{paddingLeft:"10rem",paddingRight:"10rem"}}>
+        <Grid.Row style={{ paddingLeft: '10rem', paddingRight: '10rem' }}>
           <Button secondary as={Link} to={'/createPost'}>
             Create Post
           </Button>
         </Grid.Row>
       )}
-        {loading ? (
-          <h1>Loading posts..</h1>
-        ) : (
-          <Transition.Group>
-            {posts &&
-              posts.map((post) => (
-                <Grid.Row  key={post.id} style={{ marginBottom: 20, padding:"0px 10rem" }}>
-                  <PostCard post={post} />
-                </Grid.Row>
-              ))}
-          </Transition.Group>
-        )}
-    </Grid  >
+      {loading ? (
+        <h1>Loading posts..</h1>
+      ) : (
+        <Transition.Group>
+          {posts &&
+            posts.map((post) => (
+              <Grid.Row
+                key={post.id}
+                style={{ marginBottom: 20, padding: '0px 10rem' }}
+              >
+                <PostCard post={post} />
+              </Grid.Row>
+            ))}
+        </Transition.Group>
+      )}
+    </Grid>
   );
 }
 
