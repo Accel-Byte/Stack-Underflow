35d138065e7f9d09b426b4b217664d3483d4b49e
d3v13 <patelcheris13@gmail.com>
2021-05-01 22:38:08 +0530

    Error handling and added NothingHere for post not found

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,10 @@
 import React, { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-import { Grid, Transition, Button } from 'semantic-ui-react';
+import { Grid, Transition, Button, Loader } from 'semantic-ui-react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+  
 
 import { AuthContext } from '../context/auth';
 import PostCard from '../components/PostCard/HomeCard';
@@ -10,7 +13,20 @@ import { FETCH_POSTS_QUERY, NEW_POST_SUBSCRIPTION } from '../utils/graphql';
 function Home() {
   const { user } = useContext(AuthContext);
   const { subscribeToMore, loading, data: { getPosts: posts } = {} } = useQuery(
-    FETCH_POSTS_QUERY
+    FETCH_POSTS_QUERY,
+    {
+      onError: (err) => {
+        toast.error(err.message, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          });
+      }
+    }
   );
   useEffect(() => {
     subscribeToMore({
@@ -18,18 +34,20 @@ function Home() {
       updateQuery: (prev, { subscriptionData }) => {
         if (!subscriptionData.data) return prev;
         const newPost = subscriptionData.data.newPost;
-        let updatedPosts = Object.assign({}, { getPosts: [...prev.getPosts, newPost ] });
+        let updatedPosts = Object.assign(
+          {},
+          { getPosts: [...prev.getPosts, newPost] }
+        );
         console.log(updatedPosts);
         return updatedPosts;
       },
     });
-    
-    return () => {
-      
-    }
-  }, [subscribeToMore])
+
+    return () => {};
+  }, [subscribeToMore]);
   return (
     <Grid columns={3}>
+      <ToastContainer />
       <Grid.Row className="page-title">
         <h1>Recent Posts</h1>
       </Grid.Row>
@@ -40,21 +58,18 @@ function Home() {
           </Button>
         </Grid.Row>
       )}
-      {loading ? (
-        <h1>Loading posts..</h1>
-      ) : (
-        <Transition.Group>
-          {posts &&
-            posts.map((post) => (
-              <Grid.Row
-                key={post.id}
-                style={{ marginBottom: 20, padding: '0px 10rem' }}
-              >
-                <PostCard post={post} />
-              </Grid.Row>
-            ))}
-        </Transition.Group>
-      )}
+      <Loader active={loading} size="medium" inline="centered" />
+      <Transition.Group>
+        {posts &&
+          posts.map((post) => (
+            <Grid.Row
+              key={post.id}
+              style={{ marginBottom: 20, padding: '0px 10rem' }}
+            >
+              <PostCard post={post} />
+            </Grid.Row>
+          ))}
+      </Transition.Group>
     </Grid>
   );
 }
