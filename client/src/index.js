ca11e53add66b461be10af87d5653622dbc49dd2
d3v13 <patelcheris13@gmail.com>
2021-05-30 19:34:39 +0530

    Minor Changes

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import ApolloProvider from "./ApolloProvider";
 
+import './index.css';
+
 ReactDOM.render(ApolloProvider, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
