6ef70cce0d7c401c90db7b9c6c9686e2e370b486
d3v13 <patelcheris13@gmail.com>
2021-07-04 22:18:22 +0530

    UI changes

--- client/src/components/Button/voteButton.js
+++ client/src/components/Button/voteButton.js
@@ -9,7 +9,7 @@ import MyPopup from '../../utils/MyPopup';
 function VoteButton({ user, id, voteCount, upvotes, downvotes, answerId }) {
   const [state, setState] = useState({
     vote: 0,
-    });
+  });
   useEffect(() => {
     if (user && upvotes.find((upvote) => upvote.username === user.username)) {
       setState({
@@ -63,105 +63,111 @@ function VoteButton({ user, id, voteCount, upvotes, downvotes, answerId }) {
       answerId ? dislikeAnswer() : dislikePost();
     }
   };
-
+  console.log(state.vote);
   const likeButton = user ? (
-    state.vote === 1 ? (
-      <Button
-        size="mini"
-        color="blue"
-        compact
-        style={{ paddingLeft: '1.2rem' }}
-        onClick={() => vote(1)}
-      >
-        <Icon name="thumbs up" />
-      </Button>
-    ) : (
-      <Button
-        size="mini"
-        compact
-        color="blue"
-        style={{ paddingLeft: '1.2rem' }}
-        basic
-        onClick={() => vote(1)}
-      >
-        <Icon name="thumbs up" />
-      </Button>
-    )
+    <button className="text-blue-400 focus:outline-none" onClick={() => vote(1)}>
+      {state.vote === 1 ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"
+          />
+        </svg>
+      )}
+    </button>
   ) : (
-    <Button
-      size="mini"
-      compact
-      as={Link}
-      to="/login"
-      style={{ paddingLeft: '1.2rem' }}
-      color="blue"
-      basic
-    >
-      <Icon name="thumbs up" />
-    </Button>
+    <Link to="/login">
+      <button className="text-blue-400 focus:outline-none">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"
+          />
+        </svg>
+      </button>
+    </Link>
   );
 
   const dislikeButton = user ? (
-    state.vote === -1 ? (
-      <Button
-        size="mini"
-        color="red"
-        style={{ paddingLeft: '1.2rem' }}
-        compact
-        onClick={() => vote(-1)}
-      >
-        <Icon name="thumbs down" />
-      </Button>
-    ) : (
-      <Button
-        size="mini"
-        compact
-        color="red"
-        style={{ paddingLeft: '1.2rem' }}
-        basic
-        onClick={() => vote(-1)}
-      >
-        <Icon name="thumbs down" />
-      </Button>
-    )
+    <button className="text-red-400 focus:outline-none" onClick={() => vote(-1)}>
+      {state.vote === -1 ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path d="M18 9.5a1.5 1.5 0 11-3 0v-6a1.5 1.5 0 013 0v6zM14 9.667v-5.43a2 2 0 00-1.105-1.79l-.05-.025A4 4 0 0011.055 2H5.64a2 2 0 00-1.962 1.608l-1.2 6A2 2 0 004.44 12H8v4a2 2 0 002 2 1 1 0 001-1v-.667a4 4 0 01.8-2.4l1.4-1.866a4 4 0 00.8-2.4z" />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M10 14H5.236a2 2 0 01-1.789-2.894l3.5-7A2 2 0 018.736 3h4.018a2 2 0 01.485.06l3.76.94m-7 10v5a2 2 0 002 2h.096c.5 0 .905-.405.905-.904 0-.715.211-1.413.608-2.008L17 13V4m-7 10h2m5-10h2a2 2 0 012 2v6a2 2 0 01-2 2h-2.5"
+          />
+        </svg>
+      )}
+    </button>
   ) : (
-    <Button
-      size="mini"
-      compact
-      as={Link}
-      to="/login"
-      style={{ paddingLeft: '1.2rem' }}
-      color="red"
-      basic
-    >
-      <Icon name="thumbs down" />
-    </Button>
+    <Link to="/login">
+      <button className="text-red-400 focus:outline-none">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M10 14H5.236a2 2 0 01-1.789-2.894l3.5-7A2 2 0 018.736 3h4.018a2 2 0 01.485.06l3.76.94m-7 10v5a2 2 0 002 2h.096c.5 0 .905-.405.905-.904 0-.715.211-1.413.608-2.008L17 13V4m-7 10h2m5-10h2a2 2 0 012 2v6a2 2 0 01-2 2h-2.5"
+          />
+        </svg>
+      </button>
+    </Link>
   );
 
   return (
-    <>
-      <Grid.Row>
-        <MyPopup content={state.vote === 1 ? 'Remove Like' : 'Like'}>
-          {likeButton}
-        </MyPopup>
-      </Grid.Row>
-      <Grid.Row>
-        <Label
-          basic
-          size="large"
-          horizontal
-          color="teal"
-          style={{ border: 'none' }}
-        >
-          {voteCount}
-        </Label>
-      </Grid.Row>
-      <Grid.Row>
-        <MyPopup content={state.vote === -1 ? 'Remove Dislike' : 'Dislike'}>
-          {dislikeButton}
-        </MyPopup>
-      </Grid.Row>
-    </>
+    <div className="flex flex-col gap-y-1">
+      {likeButton}
+      <div className="text-yellow-400 text-center font-semibold">{voteCount}</div>
+      {dislikeButton}
+    </div>
   );
 }
 
