907f507eb05d8747c72a0e2e550f6354f6fe5d82
d3v13 <patelcheris13@gmail.com>
2021-07-04 22:16:58 +0530

    Added create comment functionality

--- client/src/components/comment/createComment.js
+++ client/src/components/comment/createComment.js
@@ -1,60 +1,147 @@
-import React from 'react';
-import { Modal, Card, Form, Button } from 'semantic-ui-react';
+import React, { useState, useRef, Fragment } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
 
 const CreateComment = ({
   submitComment,
   comment,
-  user,
   commentChange,
   commentInputRef,
-  open,
-  setOpen,
+  commentLoading,
 }) => {
-  
+  const [open, setOpen] = useState(false);
+  const cancelButtonRef = useRef(null);
+
+  let circleCommonClasses = 'h-3 w-3 bg-card-dark rounded-full';
+
   return (
-    <Modal
-      closeIcon
-      open={open}
-      trigger={
-        user && (
-          <Button style={{background:'transparent', padding: '0 1rem', color:'blue'}}>
-            Create Comment
-            </Button>
-        )
-      }
-      onClose={() => setOpen(false)}
-      onOpen={() => setOpen(true)}
-    >
-      <Modal.Content>
-        <Card fluid>
-          <Card.Content>
-            <p>Post a comment</p>
-            <Form>
-              <div className="ui action input fluid">
-                <input
-                  type="text"
-                  placeholder="Comment.."
-                  name="comment"
-                  value={comment}
-                  onChange={(event) => commentChange(event)}
-                  ref={commentInputRef}
-                />
-                <button
-                  type="submit"
-                  className="ui button teal"
-                  disabled={comment.trim() === ''}
-                  onClick={() => {
-                    submitComment();
-                  }}
-                >
-                  Submit
-                </button>
+    <>
+      <button
+        className="text-card-blue-dark font-semibold cursor-pointer focus:outline-none"
+        onClick={() => setOpen(true)}
+      >
+        Add Comment
+      </button>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog
+          as="div"
+          static
+          className="fixed z-10 inset-0 overflow-y-auto"
+          initialFocus={cancelButtonRef}
+          open={open}
+          onClose={setOpen}
+        >
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-gray-800 bg-opacity-95 transition-opacity" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <div className="inline-block align-bottom bg-card-dark rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full border-2 border-yellow-400 font-poppins">
+                <div className="bg-card-dark px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6 text-red-600"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                        />
+                      </svg>
+                    </div>
+                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <Dialog.Title
+                        as="h3"
+                        className="text-lg leading-6 font-medium text-gray-100"
+                      >
+                        Post a comment
+                      </Dialog.Title>
+                      <form>
+                        <div className="mt-4">
+                          <textarea
+                            type="text"
+                            rows="3"
+                            placeholder="Comment.."
+                            name="comment"
+                            className="resize-none w-25rem focus:outline-none text-gray-300 flex-1 pt-2 pl-3 bg-gray-700"
+                            value={comment}
+                            onChange={(event) => commentChange(event)}
+                            ref={commentInputRef}
+                          />
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-700 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                  <button
+                    type="button"
+                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => {
+                      submitComment();
+                      setOpen(false);
+                    }}
+                  >
+                    {commentLoading ? (
+                      <div className="flex justify-center items-center py-1">
+                        <div
+                          className={`${circleCommonClasses} mr-1 animate-bounce1`}
+                        ></div>
+                        <div
+                          className={`${circleCommonClasses} mr-1 animate-bounce2`}
+                        ></div>
+                        <div
+                          className={`${circleCommonClasses} animate-bounce3`}
+                        ></div>
+                      </div>
+                    ) : (
+                      'Submit'
+                    )}
+                  </button>
+                  <button
+                    type="button"
+                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => setOpen(false)}
+                    ref={cancelButtonRef}
+                  >
+                    Cancel
+                  </button>
+                </div>
               </div>
-            </Form>
-          </Card.Content>
-        </Card>
-      </Modal.Content>
-    </Modal>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
+    </>
   );
 };
 export default CreateComment;
